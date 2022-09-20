f0f9867b92309cfa4d551bc71daad56270cd16f9
Ozan Dinç <84812525+ozan-dinc@users.noreply.github.com>
2022-09-20 15:45:39 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int firstNumber = GetNumberFromUser(1);
+int secondNumber = GetNumberFromUser(2);
+int result = 0;
+
+while (secondNumber >= 1)
+{
+    Console.WriteLine($"{firstNumber}  {secondNumber}", firstNumber); //0, -11
+    if (secondNumber % 2 == 1)
+    {
+        result += firstNumber;
+    }
+    firstNumber *= 2;
+    secondNumber /= 2;
+}
+
+Console.WriteLine($"Result : {result}");
+
+int GetNumberFromUser(byte count)
+{
+    int number;
+    string value;
+
+    do
+    {
+        Console.Write($"{count}. Sayı : ");
+        value = Console.ReadLine();
+    } while (!int.TryParse(value, out number) || number < 1);
+
+
+    return number;
+}
